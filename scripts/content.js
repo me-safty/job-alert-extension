68b4e25225f981882ae422b969f10c562802e52b
me-safty <erensafty@gmail.com>
2023-09-20 14:54:52 +0300

    get alert for the first job

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,6 @@
 {
   // {} Block used to avoid setting global variables
 
-  const knockDoor = new Audio(
-    "https://cms-artifacts.motionarray.com/content/motion-array/1297037/Knock_At_The_Door_mp3.mp3?Expires=2003734670657&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=gFm01Y~XUy5ZaV9Qbb1NkNAxBqw~W41yAd7pkZ3T4hrH64-eRgG2n2nLAvHfRZtHh32yp6uvCBZ7TMFk8-k0UJE47qfFc0vH4mmK23R9IWyp8BlXoklUvNmuIEHgjewGZ1eunytqu8bUtkyRtiUdQhICsz4CVLiAwnBeb4jvqfhk2qzwNZLLV57IrupVx2Ql5FRddSYjFxAV8T3Ibw75Ci1leB8DqAcDpLtsY~A57LGkUKypS2SThxNu1ks0Iccl87h0TJolYOYZqhvN7FCBNaKGDRnZLWq8tDYFmi2yEcmi7JO1Bw8-O7VhLwhV2r45104bdTykdL2Bk64AWx3iFQ__"
-  )
   let key
   let chatId
   let isTelegramMessagesOn
@@ -61,6 +58,7 @@
   } else if (Notification.permission === "granted") {
     alert("to use the job alert extension, please click on the page first.")
     window.addEventListener("load", function () {
+      getAlertForFirstJob(key, chatId, isTelegramMessagesOn)
       getJobAlert()
     })
   } else if (Notification.permission !== "denied") {
@@ -68,6 +66,7 @@
       if (permission === "granted") {
         alert("to use the job alert extension, please click on the page first.")
         window.addEventListener("load", function () {
+          getAlertForFirstJob(key, chatId, isTelegramMessagesOn)
           getJobAlert()
         })
       }
@@ -86,25 +85,17 @@
             ".up-card-section.up-card-list-section.up-card-hover"
           )
           if (job.id !== newJob.id) {
-            const body = getJobDescription(newJob)
-            const notification = new Notification("upwork job", {
-              body,
-            })
+            sendChromeNotification(
+              getJobDescription(newJob),
+              job.children[0].children[0].children[1].children[0].href
+            )
 
-            notification.onclick = (event) => {
-              event.preventDefault()
-              window.open(jobLink, "_blank")
-            }
-
-            const playPromise = knockDoor.play()
-            if (playPromise !== undefined) {
-              playPromise.catch(function (error) {
-                console.log(error)
-                alert("Please interact with the document first.")
-              })
-            }
-
-            telegramMessages(key, chatId, isTelegramMessagesOn, body)
+            sendTelegramMessage(
+              key,
+              chatId,
+              isTelegramMessagesOn,
+              getJobDescription(newJob)
+            )
 
             job = newJob
           }
@@ -112,53 +103,99 @@
       }, 8000)
     }
   }
-}
 
-function telegramMessages(key, chatId, isTelegramMessagesOn, notificationText) {
-  if (key && chatId && isTelegramMessagesOn) {
-    const options = {
-      method: "POST",
-      headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        text: notificationText,
-        parse_mode: "None",
-        disable_web_page_preview: false,
-        disable_notification: false,
-        reply_to_message_id: 0,
-        chat_id: chatId,
-      }),
+  function sendTelegramMessage(
+    key,
+    chatId,
+    isTelegramMessagesOn,
+    notificationText
+  ) {
+    if (key && chatId && isTelegramMessagesOn) {
+      const options = {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          text: notificationText,
+          parse_mode: "None",
+          disable_web_page_preview: false,
+          disable_notification: false,
+          reply_to_message_id: 0,
+          chat_id: chatId,
+        }),
+      }
+
+      fetch(`https://api.telegram.org/bot${key}/sendMessage`, options)
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+        .catch((err) => {
+          alert("the token or the chatId is wrong")
+          console.error(err)
+        })
+    }
+  }
+
+  function getJobDescription(job) {
+    const name = job.children[0].children[0].children[1].innerText
+    const level = job.children[1].children[0].children[0].children[1].innerText
+    const type = job.children[1].children[0].children[0].children[0].innerText
+    const prise = job.children[1].children[0].children[0].children[2].innerText
+    const rate = +[
+      ...job.children[1].children[4].children[1].children[0].children[0]
+        .children[2].children,
+    ]
+      .at(-1)
+      .innerText.split(" ")[2]
+    const country =
+      job?.children[1]?.children[4]?.children[3]?.children[1]?.innerText
+    const spent = job.children[1].children[4].children[2].children[0].innerText
+    const jobLink = job.children[0].children[0].children[1].children[0].href
+
+    return `${name}\n${level} - ${type}\n${prise} - ${
+      rate > 0 ? rate.toFixed(2) : 0
+    } star - ${country} - ${spent} \n${jobLink}`
+  }
+
+  function sendChromeNotification(body, link) {
+    const notification = new Notification("upwork job", {
+      body,
+    })
+
+    const knockDoor = new Audio(
+      "https://cms-artifacts.motionarray.com/content/motion-array/1297037/Knock_At_The_Door_mp3.mp3?Expires=2003734670657&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=gFm01Y~XUy5ZaV9Qbb1NkNAxBqw~W41yAd7pkZ3T4hrH64-eRgG2n2nLAvHfRZtHh32yp6uvCBZ7TMFk8-k0UJE47qfFc0vH4mmK23R9IWyp8BlXoklUvNmuIEHgjewGZ1eunytqu8bUtkyRtiUdQhICsz4CVLiAwnBeb4jvqfhk2qzwNZLLV57IrupVx2Ql5FRddSYjFxAV8T3Ibw75Ci1leB8DqAcDpLtsY~A57LGkUKypS2SThxNu1ks0Iccl87h0TJolYOYZqhvN7FCBNaKGDRnZLWq8tDYFmi2yEcmi7JO1Bw8-O7VhLwhV2r45104bdTykdL2Bk64AWx3iFQ__"
+    )
+    const playPromise = knockDoor.play()
+    if (playPromise !== undefined) {
+      playPromise.catch(function (error) {
+        console.log(error)
+        alert("Please interact with the document first.")
+      })
     }
 
-    fetch(`https://api.telegram.org/bot${key}/sendMessage`, options)
-      .then((response) => response.json())
-      .then((response) => console.log(response))
-      .catch((err) => {
-        alert("the token or the chatId is wrong")
-        console.error(err)
-      })
+    notification.onclick = (event) => {
+      event.preventDefault()
+      window.open(link, "_blank")
+    }
   }
-}
 
-function getJobDescription(job) {
-  const name = job.children[0].children[0].children[1].innerText
-  const level = job.children[1].children[0].children[0].children[1].innerText
-  const type = job.children[1].children[0].children[0].children[0].innerText
-  const prise = job.children[1].children[0].children[0].children[2].innerText
-  const rate = +[
-    ...job.children[1].children[4].children[1].children[0].children[0]
-      .children[2].children,
-  ]
-    .at(-1)
-    .innerText.split(" ")[2]
-  const country =
-    job?.children[1]?.children[4]?.children[3]?.children[1]?.innerText
-  const spent = job.children[1].children[4].children[2].children[0].innerText
-  const jobLink = job.children[0].children[0].children[1].children[0].href
-
-  return `${name}\n${level} - ${type}\n${prise} - ${rate.toFixed(
-    2
-  )} star - ${country} - ${spent} \n${jobLink}`
+  function getAlertForFirstJob(key, chatId, isTelegramMessagesOn) {
+    let job = document.querySelector(
+      ".up-card-section.up-card-list-section.up-card-hover"
+    )
+    if (job) {
+      sendChromeNotification(
+        getJobDescription(job),
+        job.children[0].children[0].children[1].children[0].href
+      )
+      sendTelegramMessage(
+        key,
+        chatId,
+        isTelegramMessagesOn,
+        getJobDescription(job)
+      )
+      console.log("first job")
+    }
+  }
 }
