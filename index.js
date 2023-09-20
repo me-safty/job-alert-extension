4a3329da17c08f162be796d297874c0dedeef9bd
me-safty <erensafty@gmail.com>
2023-09-20 13:32:31 +0300

    add checkBox for telegram messages

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const telegramMessagesCheckBox = document.querySelector("#telegram-messages")
+// const notificationSoundCheckBox = document.querySelector("#notification-sound")
 
 const addTelegramApiSection = document.querySelector("section")
 const btn = document.querySelector("button")
 const apiInput = document.querySelector("#telegram-api-key")
 const chatIdInput = document.querySelector("#telegram-chat-id")
 
+chrome.storage.local.get(
+  [
+    "apiKey",
+    "chatId",
+    "isTelegramMessagesOn",
+    // , "isNotificationSoundOn"
+  ],
+  function (items) {
+    apiInput.value = items.apiKey ? items.apiKey : ""
+    chatIdInput.value = items.chatId ? items.chatId : ""
+    telegramMessagesCheckBox.checked = items.isTelegramMessagesOn
+      ? items.isTelegramMessagesOn
+      : false
+    // notificationSoundCheckBox.checked = items.isNotificationSoundOn
+    console.log("Settings retrieved", items)
+  }
+)
+
+onCheckBoxChange(telegramMessagesCheckBox, "isTelegramMessagesOn")
+// onCheckBoxChange(notificationSoundCheckBox, "isNotificationSoundOn")
+
 btn.onclick = () => {
   if (apiInput.value && chatIdInput.value) {
-    chrome.storage.sync.set(
+    chrome.storage.local.set(
       { apiKey: apiInput.value, chatId: chatIdInput.value },
       function () {
         console.log("Settings saved")
+        alert("Settings saved")
       }
     )
   }
+}
+
+function onCheckBoxChange(checkBox, key) {
+  checkBox.addEventListener("change", () => {
+    chrome.storage.local.set({ [key]: checkBox.checked }, function () {
+      console.log(key, checkBox.checked)
+    })
+  })
 }
--- scripts/content.js
+++ scripts/content.js
@@ -1,124 +1,166 @@
 {
   // {} Block used to avoid setting global variables
 
+  const knockDoor = new Audio(
+    "https://cms-artifacts.motionarray.com/content/motion-array/1297037/Knock_At_The_Door_mp3.mp3?Expires=2003734670657&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=gFm01Y~XUy5ZaV9Qbb1NkNAxBqw~W41yAd7pkZ3T4hrH64-eRgG2n2nLAvHfRZtHh32yp6uvCBZ7TMFk8-k0UJE47qfFc0vH4mmK23R9IWyp8BlXoklUvNmuIEHgjewGZ1eunytqu8bUtkyRtiUdQhICsz4CVLiAwnBeb4jvqfhk2qzwNZLLV57IrupVx2Ql5FRddSYjFxAV8T3Ibw75Ci1leB8DqAcDpLtsY~A57LGkUKypS2SThxNu1ks0Iccl87h0TJolYOYZqhvN7FCBNaKGDRnZLWq8tDYFmi2yEcmi7JO1Bw8-O7VhLwhV2r45104bdTykdL2Bk64AWx3iFQ__"
+  )
   let key
   let chatId
+  let isTelegramMessagesOn
+  let interval
 
-  chrome.storage.sync.get(["apiKey", "chatId"], function (items) {
-    if (items.apiKey && items.chatId) {
-      key = items.apiKey
-      chatId = items.chatId
-    } else {
-      alert(
-        "you must provied the api token and the chat id to make the messages work"
+  chrome.storage.local.get(
+    [
+      "apiKey",
+      "chatId",
+      "isTelegramMessagesOn",
+      //  "isNotificationSoundOn"
+    ],
+    function (items) {
+      // isNotificationSoundOn = items.isNotificationSoundOn || false
+      isTelegramMessagesOn = items.isTelegramMessagesOn || false
+      if (isTelegramMessagesOn) {
+        if (items.apiKey && items.chatId) {
+          key = items.apiKey
+          chatId = items.chatId
+        } else {
+          alert(
+            "you must provide the api token and the chat id to make the messages work"
+          )
+        }
+      }
+      console.log(
+        key,
+        chatId,
+        isTelegramMessagesOn,
+        // isNotificationSoundOn,
+        items
       )
     }
-    console.log(key, chatId, items)
-  })
+  )
 
   chrome.storage.onChanged.addListener((changes, namespace) => {
     console.log(changes, namespace, "key", key, "chat", chatId)
 
+    isTelegramMessagesOn = changes.isTelegramMessagesOn
+      ? changes.isTelegramMessagesOn.newValue
+      : isTelegramMessagesOn
+
     key = changes.apiKey ? changes.apiKey.newValue : key
     chatId = changes.chatId ? changes.chatId.newValue : chatId
+    clearInterval(interval)
+    getJobAlert()
 
-    console.log(changes, namespace, "key", key, "chat", chatId)
+    console.log(changes, namespace)
   })
 
-  const knockDoor = new Audio(
-    "https://cms-artifacts.motionarray.com/content/motion-array/1297037/Knock_At_The_Door_mp3.mp3?Expires=2003734670657&Key-Pair-Id=K2ZDLYDZI2R1DF&Signature=gFm01Y~XUy5ZaV9Qbb1NkNAxBqw~W41yAd7pkZ3T4hrH64-eRgG2n2nLAvHfRZtHh32yp6uvCBZ7TMFk8-k0UJE47qfFc0vH4mmK23R9IWyp8BlXoklUvNmuIEHgjewGZ1eunytqu8bUtkyRtiUdQhICsz4CVLiAwnBeb4jvqfhk2qzwNZLLV57IrupVx2Ql5FRddSYjFxAV8T3Ibw75Ci1leB8DqAcDpLtsY~A57LGkUKypS2SThxNu1ks0Iccl87h0TJolYOYZqhvN7FCBNaKGDRnZLWq8tDYFmi2yEcmi7JO1Bw8-O7VhLwhV2r45104bdTykdL2Bk64AWx3iFQ__"
-  )
+  // not working
 
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification")
   } else if (Notification.permission === "granted") {
-    getJobAlert()
+    alert("to use the job alert extension, please click on the page first.")
+    window.addEventListener("load", function () {
+      getJobAlert()
+    })
   } else if (Notification.permission !== "denied") {
     Notification.requestPermission().then((permission) => {
       if (permission === "granted") {
-        getJobAlert()
+        alert("to use the job alert extension, please click on the page first.")
+        window.addEventListener("load", function () {
+          getJobAlert()
+        })
       }
     })
   }
 
   function getJobAlert() {
-    alert("to use the (job alert extention), please click on the page firsr.")
-    window.addEventListener("load", function () {
-      let job = document.querySelector(
-        ".up-card-section.up-card-list-section.up-card-hover"
-      )
-      if (job) {
-        setInterval(() => {
-          document.querySelector(".up-btn.up-btn-primary").click()
-          setTimeout((_) => {
-            let newJob = document.querySelector(
-              ".up-card-section.up-card-list-section.up-card-hover"
-            )
-            if (job.id !== newJob.id) {
-              const name = newJob.children[0].children[0].children[1].innerText
-              const level =
-                newJob.children[1].children[0].children[0].children[1].innerText
-              const type =
-                newJob.children[1].children[0].children[0].children[0].innerText
-              const prise =
-                newJob.children[1].children[0].children[0].children[2].innerText
-              const rate =
-                newJob.children[1].children[4].children[1].children[0]
-                  .children[0].children[1].children.length
-              const country =
-                newJob.children[1].children[4].children[3].children[1].innerText
-              const spent =
-                newJob.children[1].children[4].children[2].children[0].innerText
-              const jobLink =
-                newJob.children[0].children[0].children[1].children[0].href
-              const title = "upwork job"
-              const icon = ""
-              const body = `${name}\n${level} - ${type}\n${prise} - ${rate} ⭐ - ${country} - ${spent}`
-              const notification = new Notification(title, { body, icon })
-              notification.onclick = (event) => {
-                event.preventDefault()
-                window.open(jobLink, "_blank")
-              }
-
-              const playPromise = knockDoor.play()
-              if (playPromise !== undefined) {
-                playPromise.catch(function (error) {
-                  console.log(error)
-                  alert("Please interact with the document first.")
-                })
-              }
-
-              if (key && chatId) {
-                const options = {
-                  method: "POST",
-                  headers: {
-                    accept: "application/json",
-                    "content-type": "application/json",
-                  },
-                  body: JSON.stringify({
-                    text: `${name}\n${level} - ${type}\n${prise} - ${rate} ⭐ - ${country} - ${spent} \n${jobLink}`,
-                    parse_mode: "None",
-                    disable_web_page_preview: false,
-                    disable_notification: false,
-                    reply_to_message_id: 0,
-                    chat_id: chatId,
-                  }),
-                }
-
-                fetch(`https://api.telegram.org/bot${key}/sendMessage`, options)
-                  .then((response) => response.json())
-                  .then((response) => console.log(response))
-                  .catch((err) => {
-                    alert("the token or the chatid is wrong")
-                    console.error(err)
-                  })
-              }
-
-              job = newJob
+    let job = document.querySelector(
+      ".up-card-section.up-card-list-section.up-card-hover"
+    )
+    if (job) {
+      interval = setInterval(() => {
+        document.querySelector(".up-btn.up-btn-primary").click()
+        setTimeout((_) => {
+          let newJob = document.querySelector(
+            ".up-card-section.up-card-list-section.up-card-hover"
+          )
+          if (job.id !== newJob.id) {
+            const name = newJob.children[0].children[0].children[1].innerText
+            const level =
+              newJob.children[1].children[0].children[0].children[1].innerText
+            const type =
+              newJob.children[1].children[0].children[0].children[0].innerText
+            const prise =
+              newJob.children[1].children[0].children[0].children[2].innerText
+            // const rate = [
+            //   ...newJob.children[1].children[4].children[1].children[0]
+            //     .children[0].children[2].children,
+            // ].at(-1).innerText
+            const rate = "fhffh"
+            const country =
+              newJob?.children[1]?.children[4]?.children[3]?.children[1]
+                ?.innerText
+            const spent =
+              newJob.children[1].children[4].children[2].children[0].innerText
+            const jobLink =
+              newJob.children[0].children[0].children[1].children[0].href
+            const title = "upwork job"
+            const icon = ""
+            const body = `${name}\n${level} - ${type}\n${prise} - ${rate} ⭐ - ${country} - ${spent}`
+            const notification = new Notification(title, { body, icon })
+            notification.onclick = (event) => {
+              event.preventDefault()
+              window.open(jobLink, "_blank")
             }
-          }, 3500)
-        }, 13000)
-      }
-    })
+
+            const playPromise = knockDoor.play()
+            if (playPromise !== undefined) {
+              playPromise.catch(function (error) {
+                console.log(error)
+                alert("Please interact with the document first.")
+              })
+            }
+
+            telegramMessages(
+              key,
+              chatId,
+              isTelegramMessagesOn,
+              `${name}\n${level} - ${type}\n${prise} - ${rate} ⭐ - ${country} - ${spent} \n${jobLink}`
+            )
+
+            job = newJob
+          }
+        }, 3000)
+      }, 8000)
+    }
+  }
+}
+
+function telegramMessages(key, chatId, isTelegramMessagesOn, notificationText) {
+  if (key && chatId && isTelegramMessagesOn) {
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        text: notificationText,
+        parse_mode: "None",
+        disable_web_page_preview: false,
+        disable_notification: false,
+        reply_to_message_id: 0,
+        chat_id: chatId,
+      }),
+    }
+
+    fetch(`https://api.telegram.org/bot${key}/sendMessage`, options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => {
+        alert("the token or the chatId is wrong")
+        console.error(err)
+      })
   }
 }
